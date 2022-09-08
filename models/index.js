69eeddf8f6b38d7f9ef67da988e842bd300cb070
MJ_Shelton <mjshelton12@comcast.net>
2022-09-07 23:13:48 -0700

    Built database relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'catagory_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+})
 
 module.exports = {
   Product,
